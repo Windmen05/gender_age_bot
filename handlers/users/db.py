f08230a68fcbc6c88995114f9ef07725c1fbfc42
vadim <Vadimka36@yandex.ru>
2020-12-19 13:21:37 +0300

    Dell referrals from DataBase

--- handlers/users/db.py
+++ handlers/users/db.py
@@ -9,29 +9,22 @@ from loader import bot, dp, db
 
 class DBCommands:
     pool: Connection = db
-    ADD_NEW_USER_REFERRAL = "INSERT INTO users(chat_id, username, full_name, referral) " \
-                            "VALUES ($1, $2, $3, $4) RETURNING id"
+    ADD_NEW_USER_REFERRAL = "INSERT INTO users(chat_id, username, full_name) " \
+                            "VALUES ($1, $2, $3) RETURNING id"
     ADD_NEW_USER = "INSERT INTO users(chat_id, username, full_name) VALUES ($1, $2, $3) RETURNING id"
     COUNT_USERS = "SELECT COUNT(*) FROM users"
     GET_ID = "SELECT id FROM users WHERE chat_id = $1"
-    CHECK_REFERRALS = "SELECT chat_id FROM users WHERE referral=" \
-                      "(SELECT id FROM users WHERE chat_id=$1)"
     CHECK_BALANCE = "SELECT balance FROM users WHERE chat_id = $1"
     ADD_MONEY = "UPDATE users SET balance=balance+$1 WHERE chat_id = $2"
 
-    async def add_new_user(self, referral=None):
+    async def add_new_user(self):
         user = types.User.get_current()
 
         chat_id = user.id
         username = user.username
         full_name = user.full_name
         args = chat_id, username, full_name
-
-        if referral:
-            args += (int(referral),)
-            command = self.ADD_NEW_USER_REFERRAL
-        else:
-            command = self.ADD_NEW_USER
+        command = self.ADD_NEW_USER
 
         try:
             record_id = await self.pool.fetchval(command, *args)
@@ -47,15 +40,6 @@ class DBCommands:
         command = self.GET_ID
         user_id = types.User.get_current().id
         return await self.pool.fetchval(command, user_id)
-
-    async def check_referrals(self):
-        user_id = types.User.get_current().id
-        command = self.CHECK_REFERRALS
-        rows = await self.pool.fetch(command, user_id)
-        return ", ".join([
-            f"{num + 1}. " + (await bot.get_chat(user["chat_id"])).get_mention(as_html=True)
-            for num, user in enumerate(rows)
-        ])
 
     async def check_balance(self):
         command = self.CHECK_BALANCE
@@ -74,8 +58,7 @@ db = DBCommands()
 @dp.message_handler(commands=["start"])
 async def register_user(message: types.Message):
     chat_id = message.from_user.id
-    referral = message.get_args()
-    id = await db.add_new_user(referral=referral)
+    id = await db.add_new_user()
     count_users = await db.count_users()
 
     text = ""
@@ -84,14 +67,9 @@ async def register_user(message: types.Message):
     else:
         text += "Записал в базу! "
 
-    bot_username = (await bot.me).username
-    bot_link = f"https://t.me/{bot_username}?start={id}"
     balance = await db.check_balance()
     text += f"""
 Сейчас в базе {count_users} человек!
-
-Ваша реферальная ссылка: {bot_link}
-Проверить рефералов можно по команде: /referrals
 
 Ваш баланс: {balance} монет.
 
@@ -100,13 +78,6 @@ async def register_user(message: types.Message):
 
     await bot.send_message(chat_id, text)
 
-
-@dp.message_handler(commands=["referrals"])
-async def check_referrals(message: types.Message):
-    referrals = await db.check_referrals()
-    text = f"Ваши рефералы:\n{referrals}"
-
-    await message.answer(text)
 
 
 @dp.message_handler(commands=["add_money"])
